c91e78b18d15dd21f3112e3561f9dcae7e2ea58b
anhtuan0501 <anhtuan01673233538@gmail.com>
2020-09-25 16:12:29 +0700

    Home-Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,7 +61,17 @@ export class AppComponent implements OnInit {
         this.createNode('node7', 'vendor/json2.js');
         this.createNode('node8', 'js/google.js');
         this.createNode('node11', 'main.js');
-        this.createNode('node11', 'script.js');
+        this.createNode('node12', 'script.js');
+        this.createNode('node13', 'bootstrap.js');
+        this.createNode('node14', 'classie-search.js');
+        this.createNode('node15', 'demo1-search.js');
+        this.createNode('node16', 'easing.js');
+        this.createNode('node17', 'jquery.countup.js');
+        this.createNode('node18', 'jquery.waypoints.min.js');
+        this.createNode('node19', 'jquery.zoomslider.min.js');
+        this.createNode('node20', 'jquery-1.11.1.min.js');
+        this.createNode('node21', 'jquery-2.2.3.min.js');
+        this.createNode('node22', 'modernizr-2.6.2.min.js');
       }
     });
   }
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -6,3 +6,56 @@ var all_spc = document.querySelectorAll("form[id='202671468576465'] .si" + "mple
 for (var i = 0; i < all_spc.length; i++) {
   all_spc[i].value = "202671468576465-202671468576465";
 }
+
+$(document).ready(function() {
+  $(".dropdown").hover(
+    function() {
+      $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+      $(this).toggleClass('open');
+    },
+    function() {
+      $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+      $(this).toggleClass('open');
+    }
+  );
+});
+
+window.onload = function() {
+  document.getElementById("password1").onchange = validatePassword;
+  document.getElementById("password2").onchange = validatePassword;
+}
+
+function validatePassword() {
+  var pass2 = document.getElementById("password2").value;
+  var pass1 = document.getElementById("password1").value;
+  if (pass1 != pass2)
+    document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+  else
+    document.getElementById("password2").setCustomValidity('');
+  //empty string means no validation error
+}
+
+jQuery(document).ready(function($) {
+  $(".scroll").click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 900);
+  });
+});
+
+$(document).ready(function() {
+  /*
+              var defaults = {
+                  containerID: 'toTop', // fading element id
+                containerHoverID: 'toTopHover', // fading element hover id
+                scrollSpeed: 1200,
+                easingType: 'linear'
+               };
+              */
+
+  $().UItoTop({
+    easingType: 'easeOutQuart'
+  });
+
+});
