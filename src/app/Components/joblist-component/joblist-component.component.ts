61073844cdda75b15975a2d18fdeac3c13bf4e15
conboli9 <conboli9>
2020-10-01 14:05:00 +0700

    new update 1/10

--- src/app/Components/joblist-component/joblist-component.component.ts
+++ src/app/Components/joblist-component/joblist-component.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-joblist-component',
   templateUrl: './joblist-component.component.html',
-  styleUrls: ['./joblist-component.component.css']
+  styleUrls: ['./joblist-component.component.css'],
 })
 export class JoblistComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
