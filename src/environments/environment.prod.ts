0f5e4eb1572bd90722573075e4df4e441910d28d
anhtuan0501 <anhtuan01673233538@gmail.com>
2020-10-13 00:19:22 +0700

    updates to deploy to heroku1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCRgB5lwTBkhjGh26g2cuP-v4tjH0qupPw",
+    authDomain: "website-cv-44847.firebaseapp.com",
+    databaseURL: "https://website-cv-44847.firebaseio.com",
+    projectId: "website-cv-44847",
+    storageBucket: "website-cv-44847.appspot.com",
+    messagingSenderId: "758193052647",
+    appId: "1:758193052647:web:e603031f8d1e0b8cf33b42",
+    measurementId: "G-QMPF3ERNKF"
+  }
 };
